d3f1736bcaafb7c8f06ed16a817551281158359b
Eyad <s444003279@uqu.edu.sa>
2024-10-28 20:17:53 +0300

    spring boot first commit

---
+++ src/test/java/com/example/testing_project/TestingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testing_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
